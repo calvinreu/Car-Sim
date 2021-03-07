42dc5ee6ecb68f827cf94b8aca6cb2a96fd28cf1
calvinreu <j.calvin.reu@protonmail>
2021-03-07 17:06:45 +0100

    added func to get closest point of intersection

--- interface/math.cpp
+++ interface/math.cpp
@@ -1,5 +1,13 @@
 #include "math.hpp"
 
+bool DoIntersect(const linear_function &first, const linear_function &second) { return first.m != second.m || first.c == second.c; }
+bool DoAlwaysIntersect(const linear_function &first, const linear_function & second) { return first.m == second.m && first.c == second.c; }
+bool IsInRange(const double &value, const double &max, const double &min){ return value < max && min < value; }
+double GetSloope(const double &angle) { return cos(angle)/sin(angle); }
+double GetPositive(const double &value) { return value < 0 ? -1*value : value; }
+
+double GetVectorLenght(const pair &vector) { return sqrt((vector.x*vector.x)+(vector.y*vector.y)); }
+
 pair& VectorFromAngle(const double &angle, const double &length) {
     pair vector;
     vector.x = sin(angle);
@@ -10,8 +18,6 @@ pair& VectorFromAngle(const double &angle, const double &length) {
 
     return vector;
 }
-
-bool DoIntersect(const linear_function &first, const linear_function &second) { return first.m != second.m || first.c == second.c; }
 
 pair& IntersectionPoint(const linear_function &first, const linear_function &second) {
     pair position;
@@ -25,4 +31,74 @@ pair& IntersectionPoint(const linear_function &first, const linear_function &sec
     return position;
 }
 
-bool IsInRange(const double &value, const double &max, const double &min){ return value < max && min < value; }
+double DoIntersect(const double angle, const SDL_Point position, std::vector<SDL_Point> lines) {
+    linear_function main_line;
+    linear_function temp_line;
+    pair temp_vec;
+    double temp_distance;
+    double distance = max_distance; //smallest distance
+    main_line.m = GetSloope(angle);
+    main_line.c = position.y - (main_line.m*position.x);
+
+    for(auto i = lines.begin() + 1; i < lines.end(); i++) {
+        temp_line.m = (i->x - (i-1)->x)/(i->y - (i-1)->y);
+        temp_line.c = i->y - (temp_line.m * i->x);
+        if (DoIntersect(main_line, temp_line)) {
+            if(DoAlwaysIntersect(main_line, temp_line)) {
+                
+                //check if postion is on line
+                if(i->x > position.x) {
+                    if((i-1)->x <= position.x)
+                        return 0;
+                }else if(i->x < position.x){
+                    if((i-1)->x >= position.x)
+                        return 0;
+                }else{
+                    return 0;//point position is on line
+                }
+
+                temp_vec.x = GetPositive(i->x - position.x);
+                temp_vec.y = GetPositive((i-1)->x - position.x);
+
+                if(temp_vec.x < temp_vec.y){
+                    temp_vec.x = temp_vec.x;
+                    temp_vec.y = i->y - position.y;
+                }else{
+                    temp_vec.x = temp_vec.y;
+                    temp_vec.y = (i-1)->y - position.y;
+                }
+            }else{
+                temp_vec = IntersectionPoint(main_line, temp_line);
+                temp_vec.x -= position.x;
+                temp_vec.y -= position.y;//distance vector from position to IntersectionPoint
+            }
+            
+            //check if point is in the direction of angle
+            if(cos(angle) > 0) {
+                if(temp_vec.y > 0) {
+                    temp_distance = GetVectorLenght(temp_vec);
+                    if(temp_distance < distance)
+                        distance = temp_distance;
+                }
+            }else if(cos(angle) < 0) {
+                if(temp_vec.y < 0) {
+                    temp_distance = GetVectorLenght(temp_vec);//check which of the two points is closer and in the direction of anglee = GetVectorLenght(temp_vec);
+                    if(temp_distance < distance)
+                        distance = temp_distance;
+                }
+            }else if(sin(angle) > 0) {
+                if(temp_vec.x > 0) {
+                    temp_distance = GetVectorLenght(temp_vec);
+                    if(temp_distance < distance)
+                        distance = temp_distance;
+                }
+            }else{
+                if(temp_vec.x < 0) {
+                    temp_distance = GetVectorLenght(temp_vec);
+                    if(temp_distance < distance)
+                        distance = temp_distance;
+                }
+            }
+        }
+    }
+}
--- math.hpp
+++ math.hpp
@@ -1,4 +1,9 @@
+#pragma once
 #include <math.h>
+#include <SDL2/SDL.h>
+#include <vector>
+
+const double max_distance = 100;//max detectable distance for sensor
 
 
 struct pair{
@@ -20,7 +25,11 @@ extern pair& VectorFromAngle(const double &angle, const double &length);
 /**
  * @brief check if two linear functions do intersect
 **/
-extern bool DoInersect(const linear_function &first, const linear_function &second);
+extern bool DoIntersect(const linear_function &first, const linear_function &second);
+/**
+ * @brief check if two functions always intersect
+**/
+extern bool DoAlwaysInersect(const linear_function &first, const linear_function &second);
 /**
  * @brief calculate where two linear functions interect NOTE: check if they DoIntersect before using this function
  * @return location where the two functions intersect
@@ -32,3 +41,24 @@ extern pair& IntersectionPoint(const linear_function &first, const linear_functi
  * @param min is the minimum for value
 **/
 extern bool IsInRange(const double &value, const double &max, const double &min);
+
+/**
+ * @brief get the sloope from an angle
+ * @param angle the angle of the sloope
+ * @return the sloope m in a linear function
+**/
+extern double GetSloope(const double &angle);
+
+/**
+ * @brief check the closest intersection a line with other lines
+ * @param angle is the angle of the line on which the distance is measured
+ * @param position is the start position of the line on which the distance is measured
+ * @param lines are the lines to which the distance is measured
+ * @return the closest non negative distance to a line if it doesn't intersect any it will return max_distance
+**/
+extern double DoIntersect(const double angle, const SDL_Point position, std::vector<SDL_Point> lines);
+
+/**
+ * @brief return difference to 0
+**/
+extern double GetPositive(const double &value);
